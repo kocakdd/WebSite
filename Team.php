4a1913b83a0344f4fda73f705ea53ae193923791
Derya Kocak <derya-kocak@live.fr>
2022-06-15 16:37:18 +0200

    debut team

--- Team.php
+++ Team.php
@@ -24,31 +24,52 @@
 <div class="content">
   <img class="desk-image" src="images/Team.png" alt="Font Image">
 
-  <div>
+  <div class="first-text-block">
     <h1 class="bigTitle1">Team</h1>
-    <h2 class="bigTitle2">Uw visie. Onze missie</h2>
+    <h2 class="bigTitle2">Uw visie. Onze missie.</h2>
   </div>
 
   <div class="second-text-block">
-    <div class="littleTitle"> Over ons </div>
+    <h2> Neem contact op </h2>
     <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
     vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
     amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
     justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-</br></br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+</br>
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
     vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
     amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
-    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
-  
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
   </div>
 
-  <div class="Kernwaarden">
-    <div class="littleTitle">Kernwaarden</div>
-    <p>orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+  <div class="third-text-block">
+    <h2> Kernwaarden </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
     vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
     amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
     justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
-
+  <div class="boxes">
+    <div class="boxe1">
+    <img class="image1" src="images/Honesty.png" alt="honesty">
+    <p class="text2">Honesty</p>
+  </div>
+  <div class="boxe2">
+    <img class="image2" src="images/data.png" alt="data">
+    <p class="text2">Data Driven</p>
+  </div>
+  <div class="boxe3">
+    <img class="image3" src="images/frontier.png" alt="frontier">
+    <p class="text2">Frontier</p>
+  </div>
+  <div class="boxe4">
+    <img class="image2" src="images/secured.png" alt="secured">
+    <p class="text2">Secured</p>
+  </div>
+  <div class="boxe5">
+    <img class="image4" src="images/digital.png" alt="digital">
+    <p class="text2">Digital</p>
+  </div>
+</div>
   </div>
 
    <!--Footer-->   
