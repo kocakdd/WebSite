8487d6a56c17c12e2386c36f494c75f79ab9a7ea
Napierala Lisa <lisa.napierala@gmail.com>
2022-06-16 10:40:42 +0200

    bouton enlevé

--- Home.php
+++ Home.php
@@ -36,10 +36,11 @@
     <h3 class="title2"> Subtitle </h3>
     <p class="text1"> J'ai modifier Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum </p>
 
-    <!-- Button -->
+    <!-- Button 
     <div class="center">
       <button onClick = "window.location.href='Max_hypotheek.php';" class="button" role="button" style="position: center">Bereken maximale hypotheek</button>
     </div>
+    -->
   </div>
 
   <div class="second-block">
