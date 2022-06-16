d2931aceeeb56aca1d8699496954fdce602c81f9
Derya Kocak <derya-kocak@live.fr>
2022-06-16 15:49:05 +0200

    fin page Team

--- Team.php
+++ Team.php
@@ -54,37 +54,136 @@ Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
   
   <div class="boxes">
   <div class="boxe">
-    <img class="logo" src="images/Honesty.svg" alt="honesty">
+    <img class="logo2" src="images/Honesty.svg" alt="honesty">
     <h2> Honesty </h2>
   </div>
 
   <div class="boxe">
-    <img class="logo" src="images/Graph.svg" alt="data">
+    <img class="logo2" src="images/Graph.svg" alt="data">
     <h2> Data Driven </h2>
   </div>
 
     <div class="boxe">
-    <img class="logo" src="images/frontier.svg" alt="frontier">
+    <img class="logo2" src="images/frontier.svg" alt="frontier">
     <h2> Frontier </h2>
   </div>
 
   <div class="boxe">
-    <img class="logo" src="images/secured.svg" alt="secured">
+    <img class="logo2" src="images/secured.svg" alt="secured">
     <h2> Secured </h2>
   </div>
 
   <div class="boxe">
-    <img class="logo" src="images/digital.svg" alt="digital">
+    <img class="logo2" src="images/digital.svg" alt="digital">
     <h2> Digital</h2>
   </div>
 </div>
 
 </div>
    
+<div class="Management-text-block">
+    <h2>Management </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+    <div class ="Management-img">
+      
 
+      <div class="crew">
+      <img class="Member" src="images/Chee.png" alt="Chee">
+      <p class="name">Chee Hang</p>
+      <p class="role">Co-Founder - Financial & Behavioral Economics</p>
+      </div>
 
+      <div class="crew">
+      <img class="Member" src="images/Phill.png" alt="Philippe">
+      <p class="name">Philippe Barendregt</p>
+      <p class="role">CEO/Co-Founder - Financial Law & Economics</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Pieter.png" alt="Pieter">
+      <p class="name">Pieter van Poecke</p>
+      <p class="role">Co-Founder - Business & Data Analytics</p>
+      </div>
+    </div>
+</div>
+
+<div class="Team-text-block">
+    <h2>Management </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+    <div class ="Team-img">
+      
+
+      <div class="crew">
+      <img class="Member" src="images/Jeff.jpg" alt="Jeffrey">
+      <p class="name">Jeffrey Thakoerdien</p>
+      <p class="role">Advisory</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Chinelo.png" alt="Chinelo">
+      <p class="name">Chinelo White</p>
+      <p class="role">Advisory</p>
+    </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Soraya.jpg" alt="Soraya">
+      <p class="name">Soraya Chiragali</p>
+      <p class="role">Advisory</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Shaf.jpg" alt="Shaf">
+      <p class="name">Shaf Bakas</p>
+      <p class="role">Law</p>
+     </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Giulia.jpg" alt="Giulia">
+      <p class="name">Giulia Montagna</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Sabine.jpg" alt="Sabine">
+      <p class="name">Sabine Geurts</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Derya.jpg" alt="Derya">
+      <p class="name">Derya Kocak</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Lisa.jpg" alt="Lisa">
+      <p class="name">Lisa Napierala</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Oscar.jpg" alt="Oscar">
+      <p class="name">Oscar Conan</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="images/Ryan.jpg" alt="Ryan">
+      <p class="name">Ryan Karia</p>
+      <p class="role">Design</p>
+      </div>
+
+    </div>
+</div>
+
+</div>
    <!--Footer-->   
    <?php include('Action/footer.php'); ?> 
-   </div>
 </body>
 </html>
