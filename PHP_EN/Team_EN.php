d78d37443180be05e1480ca86118f912dc33adc0
Derya Kocak <derya-kocak@live.fr>
2022-06-17 11:15:06 +0200

    Team EN + footer taille

---
+++ PHP_EN/Team_EN.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="EN">
+
+<head>	
+	<title> Hypotheekvitaak - Team </title>	
+	<META	NAME="author"	CONTENT="Kocak Derya">	
+	<META	NAME="description"	CONTENT="Team">	
+	<META	NAME="geography"	CONTENT="Rotterdam,	South-Holland,	Nertheland">	
+	<META	NAME="keywords"	CONTENT="Rotterdam, Team">	
+	<META	NAME="subject"	CONTENT="Team">	
+    <META  charset="UTF-8">
+    <META name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSS link -->
+  <link rel="stylesheet" href="../css/Team.css">
+
+  <!-- Icon image -->
+  <link rel="icon" href="../images/LogoHyp.svg">
+	
+</head>	
+
+
+    <!--Header-->    
+<body>  
+  <?php include_once('header_EN.php'); ?>
+
+  <!-- Content -->  
+<div class="content">
+  <img class="desk-image" src="../images/Team.png" alt="Font Image">
+
+  <div class="first-text-block">
+    <h1 class="bigTitle1">Team</h1>
+    <h2 class="bigTitle2">Your vision. Our mission.</h2>
+  </div>
+
+  <div class="second-text-block">
+    <h2> Contact Us </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+</br>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  </div>
+
+  <div class="third-text-block">
+    <h2> Core values </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+  
+  <div class="boxes">
+  <div class="boxe">
+    <img class="logo2" src="../images/Honesty.svg" alt="honesty">
+    <h2> Honesty </h2>
+  </div>
+
+  <div class="boxe">
+    <img class="logo2" src="../images/Graph.svg" alt="data">
+    <h2> Data Driven </h2>
+  </div>
+
+    <div class="boxe">
+    <img class="logo2" src="../images/frontier.svg" alt="frontier">
+    <h2> Frontier </h2>
+  </div>
+
+  <div class="boxe">
+    <img class="logo2" src="../images/secured.svg" alt="secured">
+    <h2> Secured </h2>
+  </div>
+
+  <div class="boxe">
+    <img class="logo2" src="../images/digital.svg" alt="digital">
+    <h2> Digital</h2>
+  </div>
+</div>
+
+</div>
+   
+<div class="Management-text-block">
+    <h2>Management </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+    <div class ="Management-img">
+      
+
+      <div class="crew">
+      <img class="Member" src="../images/Chee.png" alt="Chee">
+      <p class="name">Chee Hang</p>
+      <p class="role">Co-Founder - Financial & Behavioral Economics</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Phill.png" alt="Philippe">
+      <p class="name">Philippe Barendregt</p>
+      <p class="role">CEO/Co-Founder - Financial Law & Economics</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Pieter.png" alt="Pieter">
+      <p class="name">Pieter van Poecke</p>
+      <p class="role">Co-Founder - Business & Data Analytics</p>
+      </div>
+    </div>
+</div>
+
+<div class="Team-text-block">
+    <h2>Management </h2>
+    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+    <div class ="Team-img">
+      
+
+      <div class="crew">
+      <img class="Member" src="../images/Jeff.jpg" alt="Jeffrey">
+      <p class="name">Jeffrey Thakoerdien</p>
+      <p class="role">Advisory</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Chinelo.png" alt="Chinelo">
+      <p class="name">Chinelo White</p>
+      <p class="role">Advisory</p>
+    </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Soraya.jpg" alt="Soraya">
+      <p class="name">Soraya Chiragali</p>
+      <p class="role">Advisory</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Shaf.jpg" alt="Shaf">
+      <p class="name">Shaf Bakas</p>
+      <p class="role">Law</p>
+     </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Giulia.jpg" alt="Giulia">
+      <p class="name">Giulia Montagna</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Sabine.jpg" alt="Sabine">
+      <p class="name">Sabine Geurts</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Derya.jpg" alt="Derya">
+      <p class="name">Derya Kocak</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Lisa.jpg" alt="Lisa">
+      <p class="name">Lisa Napierala</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Oscar.jpg" alt="Oscar">
+      <p class="name">Oscar Conan</p>
+      <p class="role">Data Science</p>
+      </div>
+
+      <div class="crew">
+      <img class="Member" src="../images/Ryan.jpg" alt="Ryan">
+      <p class="name">Ryan Karia</p>
+      <p class="role">Design</p>
+      </div>
+
+    </div>
+</div>
+
+</div>
+   <!--Footer-->   
+   <?php include('footer_EN.php'); ?> 
+</body>
+</html>
