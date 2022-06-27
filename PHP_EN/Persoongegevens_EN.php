16252c95730c8abd0ce76eeac47477b7a28b2cb1
Derya Kocak <derya-kocak@live.fr>
2022-06-27 16:46:33 +0200

    petites modif sur mdp oublies

---
+++ PHP_EN/Persoongegevens_EN.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="EN">
+
+<head>	
+	<title> Hypotheekvitaal - Personal Information </title>	
+	<META	NAME="author"	CONTENT="Kocak Derya">	
+	<META	NAME="description"	CONTENT="Hypotheek">	
+	<META	NAME="geography"	CONTENT="Rotterdam,	South-Holland,	Nertheland">	
+	<META	NAME="keywords"	CONTENT="Rotterdam, Hypotheek">	
+	<META	NAME="subject"	CONTENT="Hypotheek">	
+
+  <!-- CSS link -->
+	<link rel="stylesheet" href="../css/Persoongegevens.css">
+
+  
+	
+</head>	
+
+<body>
+
+    <!-- Header -->
+
+  <!-- PHP - Link with the DataBase-->
+  <?php require('../Action/db_contact.php'); ?>
+
+
+   <!-- Body  -->
+
+  <!-- htmlspecialchars allow to have the character < and no execution of html code -->
+  <div class ="user_info">
+    
+    <p class="label1"> Personal Informations </p>
+    <div class="row">
+    <div class="col first">
+      <ul>
+    <li><p class="label2"> Name</p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['voornaam'])." ".htmlspecialchars($_SESSION['achternaam']) ?></p></li>
+
+    <li><p class="label2"> Address </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['adres'])."  ".htmlspecialchars($_SESSION['woonplaats'])."  ".htmlspecialchars($_SESSION['postcode']) ?></p></li>
+
+    <li><p class="label2"> E-mail  </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['email'])?></p><li>
+  </ul></div>
+    <div class="col">
+    <ul>
+    <li><p class="label2"> Age </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['leeftijd']) ?></p></li>
+
+    <li><p class="label2"> Salary </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['loon']) ?></p></li>
+
+    <li><p class="label2"> Phone number </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['telefonnummer']) ?></p></li>
+    <li><p onclick="window.location.href='wachtwoord_wijzig.php';" class="link"> Change your password </p></li>
+  </ul>
+  </div>
+
+    <div class="col">
+      <ul>
+    <li><p class="label2"> Name Partner </p></li>
+    <li><p class="info"><?=htmlspecialchars( $_SESSION['pvoornaam'])."  ".htmlspecialchars($_SESSION['pachternaam']) ?></p></li>
+
+    <li><p class="label2"> Status Partner </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['status']) ?></p></li>
+
+    <li><p class="label2"> Mortgage </p></li>
+    <li><p class="info"><?= htmlspecialchars($_SESSION['hypotheek']) ?></p></li>
+</ul>
+    </div>
+    </div>
+    
+    
+  </div>
+
+
+</body>
