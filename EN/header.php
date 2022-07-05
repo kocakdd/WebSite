d634cfcdf6e78c4db8f0f5efc1a64714f1053bf7
Napierala Lisa <lisa.napierala@gmail.com>
2022-07-05 14:58:37 +0200

    only the V cap

--- EN/header.php
+++ EN/header.php
@@ -39,7 +39,7 @@
         <ul class="logo_menu">
           <li> <a href="Home.php"><img src="../images/LogoHyp.svg" alt="Logo Compagny"></a></li>
           <li class="text_logo"> <a href="Home.php"> 
-            <h2> HYPOTHEEKVITAAL </h2>
+            <h2> HypotheekVitaal </h2>
             <p> Ready for an house</p> 
           </a></li>
         </ul>
